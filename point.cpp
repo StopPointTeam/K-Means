e5e32eabe8ffde54a0d536a83f2887656c17ad73
3190102976@zju.edu.cn <3190102976@zju.edu.cn>
2021-01-02 23:33:57 +0800

    新增 Point 类实现

--- point.cpp
+++ point.cpp
@@ -1,1 +1,34 @@
 #include "point.h"
+
+
+Point::Point(double x, double y)
+{
+    this->x = x;
+    this->y = y;
+
+    cluster_id = -1;
+}
+
+
+double Point::GetX(void)
+{
+    return x;
+}
+
+
+double Point::GetY(void)
+{
+    return y;
+}
+
+
+double Point::GetClusterID(void)
+{
+    return cluster_id;
+}
+
+
+void Point::SetClusterID(int cluster_id)
+{
+    this->cluster_id = cluster_id;
+}
