fc8c78e4d3728f76411675aab919d769b743e4d3
3190104610@zju.edu.cn <3190104610@zju.edu.cn>
2021-01-02 23:39:37 +0800

    完成了 Cluster 类

--- cluster.cpp
+++ cluster.cpp
@@ -1,1 +1,32 @@
 #include "cluster.h"
+
+
+Cluster::Cluster(double x, double y)
+{
+    this->x = x;
+    this->y = y;
+}
+
+
+double Cluster::GetX(void)
+{
+    return x;
+}
+
+
+double Cluster::GetY(void)
+{
+    return y;
+}
+
+
+void Cluster::SetX(double x)
+{
+    this->x = x;
+}
+
+
+void Cluster::SetY(double y)
+{
+    this->y = y;
+}
