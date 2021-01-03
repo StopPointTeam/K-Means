2799464b1fc2eebdd8ae9c422f16fa8c67dc77bc
lxbpxylps@126.com <lxbpxylps@126.com>
2021-01-03 15:25:21 +0800

    实现了点和聚类的随机初始化

--- kmeans.cpp
+++ kmeans.cpp
@@ -14,3 +14,34 @@ KMeans::KMeans(int point_num, double max_point_crood, int cluster_num, int max_i
 
     srand((unsigned int)time(NULL)); //设置随机种子
 }
+
+
+//初始化点
+void KMeans::InitPoints(void)
+{
+    for (int i = 0; i < point_num; i++)
+    {
+        Point point((double)rand() / (double)RAND_MAX * max_point_crood, 
+                    (double)rand() / (double)RAND_MAX * max_point_crood);
+    
+        points.push_back(point);
+    }
+}
+
+
+//初始化聚类（均值点）
+void KMeans::InitClusters(void)
+{
+    for (int i = 0; i < cluster_num; i++)
+    {
+        Cluster cluster((double)rand() / (double)RAND_MAX * max_point_crood,
+                        (double)rand() / (double)RAND_MAX * max_point_crood);
+        
+        clusters.push_back(cluster);
+
+        next_clusters_point_num.push_back(0); //设置 next_clusters_point_num 初始值为 0
+        
+        next_clusters_x_sum.push_back(0.0); //设置 next_clusters_x_sum 初始值为 0.0
+        next_clusters_y_sum.push_back(0.0); //设置 next_clusters_y_sum 初始值为 0.0
+    }
+}
