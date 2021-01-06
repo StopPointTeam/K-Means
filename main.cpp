e67e608fc74b86ccd2bdbdfa5257a12421066bbf
lxbpxylps@126.com <lxbpxylps@126.com>
2021-01-06 08:46:42 +0800

    新增计时功能

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <chrono>
 #include <fstream>
 
 #include "kmeans.h"
@@ -7,6 +8,7 @@
 
 
 using namespace std;
+using namespace std::chrono;
 
 
 int main()
@@ -18,7 +20,15 @@ int main()
 
 	KMeans km(500000, 1000.0, 20, 20);
 
+	cout << "开始计时..." << endl;
+	time_point<system_clock> time_start = system_clock::now(); //获取计算开始时的时间
+
 	vector<Point> points = km.Result(); //将计算结果存入 points
+
+	time_point<system_clock> time_stop = system_clock::now(); //获取计算结束时的时间
+	cout << "计算结束" << endl;
+	duration<double> calc_time = time_stop - time_start; //时间差
+	cout << "time: " << calc_time.count() << "s" << endl;
 
 	ofstream fileout("data.csv"); //将结果写入 data.csv
 	
