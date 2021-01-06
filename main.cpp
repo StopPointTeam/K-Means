9089223daf86ac8cf15761448ca342d57840ff6d
lxbpxylps@126.com <lxbpxylps@126.com>
2021-01-06 08:37:40 +0800

    完成 main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+
 #include "kmeans.h"
 #include "point.h"
 
@@ -7,7 +11,21 @@ using namespace std;
 
 int main()
 {
+	cout << "点数：500000" << endl;
+	cout << "坐标最大值：1000.0" << endl;
+	cout << "聚类数：20" << endl;
+	cout << "最大迭代次数：20" << endl;
+
+	KMeans km(500000, 1000.0, 20, 20);
+
+	vector<Point> points = km.Result(); //将计算结果存入 points
+
+	ofstream fileout("data.csv"); //将结果写入 data.csv
 	
+	for (int i = 0; i < (int)points.size(); i++)
+		fileout << points[i].GetX() << "," << points[i].GetY() << "," << points[i].GetClusterID() << endl; //采用逗号分隔符
+
+	fileout.close();
 
 	return 0;
 }
